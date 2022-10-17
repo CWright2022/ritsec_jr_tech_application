88aea8dd16bc44a5d6720ba59b571120d5e7f9c0
CWright2022 <61096126+CWright2022@users.noreply.github.com>
2022-10-16 22:17:56 -0400

    this code is now cursed

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
-PASSWORD = "IabsolutelyLOVEpuppies!!!"
-def main():
+import base64
+perrytheplatypus = 'SWFic29sdXRlbHlMT1ZFcHVwcGllcyEhIQ=='
+bobthebuilder = perrytheplatypus.encode('ascii')
+system_properties = base64.b64decode(bobthebuilder)
+wifi_manager = system_properties.decode('ascii')
+drheinzdoofenshmirtz = 'UlN7QkBDS1dAUkQkXyROQEszJH0='
+python_version = drheinzdoofenshmirtz.encode('ascii')
+def run_my_app_plz_computer():
     print("welcome 2 my awesome app - by billy the kid")
     print("enter da password to continue")
-    user_input=input()
-    if user_input==PASSWORD:
+    user_input = input()
+    if user_input == wifi_manager:
         print("congrajulashuns, you win")
-        print("RS{B@CKW@RD$_$N@K3$}")
+        print(radio_frequency)
     else:
         print("nope, incorrect.")
-
-if __name__=="__main__":
-    main()
+wystem_architecture = base64.b64decode(python_version)
+radio_frequency = wystem_architecture.decode('ascii')
+run_my_app_plz_computer()
