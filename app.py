410db5e5b7b445b1e825e8ffb68caf133a579517
CWright2022 <61096126+CWright2022@users.noreply.github.com>
2022-10-16 22:02:58 -0400

    super dumb app written

---
+++ app.py
@@ -0,0 +1,13 @@
+PASSWORD = "IabsolutelyLOVEpuppies!!!"
+def main():
+    print("welcome 2 my awesome app - by billy the kid")
+    print("enter da password to continue")
+    user_input=input()
+    if user_input==PASSWORD:
+        print("congrajulashuns, you win")
+        print("RS{B@CKW@RD$_$N@K3$}")
+    else:
+        print("nope, incorrect.")
+
+if __name__=="__main__":
+    main()
